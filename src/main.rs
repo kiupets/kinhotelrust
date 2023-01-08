dbaf28b4c4c71a952e9e2f41448623e9d6c24e58
gonzalo gil <gonzalogg1975@gmail.com>
2023-01-08 11:37:04 -0300

    im god

--- src/main.rs
+++ src/main.rs
@@ -30,9 +30,8 @@ async fn main() -> std::io::Result<()> {
     let server = websocket::Server::new().start();
     let server2 = websocket2::Server2::new().start();
 
-    // let chat_server = Lobby::new().start();
-    // let HOST = env::var("HOST").expect("Host not set");
-    // let PORT = env::var("PORT").expect("Port not set");
+    let HOST = env::var("HOST").expect("Host not set");
+    let PORT = env::var("PORT").expect("Port not set");
 
     let db = MongoRepo::init().await;
     let db_data = Data::new(db);
@@ -58,8 +57,8 @@ async fn main() -> std::io::Result<()> {
             .service(Files::new("/", "./build").index_file("index.html"))
     })
     .workers(2)
-    // .bind(format!("{}:{}", HOST, PORT))?
-    .bind("127.0.0.1:8000")?
+    .bind(format!("{}:{}", HOST, PORT))?
+    // .bind("127.0.0.1:8000")?
     .run()
     .await
 }
