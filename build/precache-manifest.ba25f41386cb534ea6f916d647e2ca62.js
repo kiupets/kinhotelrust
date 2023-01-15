cd256b2e77065858dfa4b98a137db11955fd4220
gonzalo gil <gonzalogg1975@gmail.com>
2023-01-15 18:42:20 -0300

    heloo

---
+++ build/precache-manifest.ba25f41386cb534ea6f916d647e2ca62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0ae835a62e7968025ac",
+    "url": "/static/css/main.e504242a.chunk.css"
+  },
+  {
+    "revision": "f0ae835a62e7968025ac",
+    "url": "/static/js/main.4a621d3a.chunk.js"
+  },
+  {
+    "revision": "e155663ee1cd972ab8f0",
+    "url": "/static/js/runtime~main.799404a9.js"
+  },
+  {
+    "revision": "4b23034718b7a47ebde8",
+    "url": "/static/js/2.620aa3c2.chunk.js"
+  },
+  {
+    "revision": "c145123a1857c4acd0cc",
+    "url": "/static/js/3.cf3e6658.chunk.js"
+  },
+  {
+    "revision": "129beb47173723f542fd61f5a6e086fe",
+    "url": "/index.html"
+  }
+];
